36be999a5b22210d49cb4e0dfb0a088dfa019bc3
Jaber Abbas <jaber.h.abbas@gmail.com>
2024-06-30 21:47:25 +0200

    added CustomHeaderAuthenticator

--- src/main/scala/com/eu/auth/CustomHeaderAuthenticator.scala
+++ src/main/scala/com/eu/auth/CustomHeaderAuthenticator.scala
@@ -4,7 +4,7 @@ import com.eu.base64.Base64Decoder
 
 class CustomHeaderAuthenticator extends HeaderAuthenticator{
   override def extractUsername(request: ServerHttpRequest): Option[String] = {
-    val token = request.getToken
+    val token = request.getToken //replace with the ServerHttpRequest method to get the token
     val sub = Base64Decoder.getValue(token, "sub")
     Option(sub)
   }
